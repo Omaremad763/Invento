bbd0f22c0954b1e7eb381fbea716a754b47f8350
Omarowf <Omarowf@DESKTOP-8V8NS93>
2026-01-05 10:05:51 +0200

    Make DTO-internalService-AutoMapper in Application layer

---
+++ Application/AutoMapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Application.DTOS;
+
+using AutoMapper;
+
+using Domain.Entites;
+
+namespace Application
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            // هنا تعمل جميع الـ DTOs ↔ Entities
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Supplier, SupplierDto>().ReverseMap();
+        }
+    }
+}
---
+++ Application/DTOS/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOS
+{
+    public record ProductDto(
+      Guid Id,
+      string Name,
+      string SKU,
+      decimal Price,
+      int StockQuantity,
+      Guid CategoryId
+  );
+}
