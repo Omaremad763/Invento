8435debaed1d725260c956da918fe5772ebb20f9
Omar Emad Abusaif <oabuseif125@gmail.com>
2026-01-09 13:17:40 +0200

    Reshape Infra Layer

---
+++ Infrastructure/Contracts Implemintaion/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Application.Contracts;
+using Application.DTOS;
+
+using AutoMapper;
+
+using Domain.Entites;
+
+namespace Application.Internal_Services_implementation
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync()
+        {
+            var categories = await _unitOfWork.Categories.GetAllAsync();
+            return _mapper.Map<IEnumerable<CategoryDto>>(categories);
+        }
+    }
+}
--- Infrastructure/Contracts Implemintaion/RedisCacheService.cs
+++ Infrastructure/Contracts Implemintaion/RedisCacheService.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
+
+using Application.Contracts;
+
+using StackExchange.Redis;
 
 namespace Infrastructure.External_Services
 {
-    using StackExchange.Redis;
-
-    using System.Text.Json;
-
-    public interface IRedisCacheService
-    {
-        Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
-        Task<T?> GetAsync<T>(string key);
-        Task RemoveAsync(string key);
-    }
-
+ 
     public class RedisCacheService : IRedisCacheService
     {
         private readonly IDatabase _db;
