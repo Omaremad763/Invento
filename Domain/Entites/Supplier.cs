d73367c1d322abd59cd10aa110045fb518897c34
Omar Emad Abusaif <oabuseif125@gmail.com>
2026-04-08 01:09:06 +0200

    fix constructor error

--- Domain/Entites/Supplier.cs
+++ Domain/Entites/Supplier.cs
@@ -1,12 +1,11 @@
 ﻿namespace Domain.Entites
 {
-    public class Supplier(string name, string contactEmail, string? phoneNumber = null) : BaseEntity
+    public class Supplier : BaseEntity
     {
         public Guid Id { get; private set; } = Guid.NewGuid();
-        public string Name { get; private set; } = name;
-        public string ContactEmail { get; private set; } = contactEmail;
-        public string? PhoneNumber { get; private set; } = phoneNumber;
-
+        public string Name { get; private set; } 
+        public string ContactEmail { get; private set; } 
+        public string? PhoneNumber { get; private set; } 
         public string Vatstatus { get; set; }
         public bool IsDeleted { get; set; }
 
