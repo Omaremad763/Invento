b607d08d2042955575e7d525edde0a6bd6a29b10
Omar Emad Abusaif <oabuseif125@gmail.com>
2026-04-13 09:34:10 +0200

    fix CI/CD cycle

--- Front/src/app/environment.ts
+++ Front/src/app/environment.ts
@@ -1,7 +1,6 @@
 const isDockerPort = window.location.port === '4200';
 export const environment = {
   production: false,
-  apiUrl: isDockerPort ? 'http://localhost:8080/api' : 'https://localhost:44326/api',
-
+  apiUrl: '/api',
   githubID: 'Ov23ct6opUE5dP6aZBsn',
 };
